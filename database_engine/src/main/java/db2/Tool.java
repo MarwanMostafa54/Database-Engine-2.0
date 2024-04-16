53f0a1c2bd83be7d1c433b2edea1531c7b2b0f97
MarwanMostafa54 <150360991+MarwanMostafa54@users.noreply.github.com>
2024-04-16 04:30:39 +0200

    delete page

--- database_engine/src/main/java/db2/Tool.java
+++ database_engine/src/main/java/db2/Tool.java
@@ -80,21 +80,7 @@ public class Tool {
         }
     }
 
-    public static Table deserializeTable(String tableName) {
-        Table table = null;
-        try {
-            String path = "Tables/" + tableName + "/" + tableName + "_Properties.ser";
-            path = path.replaceAll("[^a-zA-Z0-9()_./+]", "");
-            FileInputStream fileAccess = new FileInputStream(path);
-            ObjectInputStream objectAccess = new ObjectInputStream(fileAccess);
-            table = (Table) objectAccess.readObject();
-            objectAccess.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Failed to deserialize table.");
-        }
-        return table;
-    }
+   
     
 
     public static void serializePage(Table T,Page P) {
