15eb3955622d3d5178beaa585958201c24091902
sp4rkii <ahmed.labib@student.guc.edu.eg>
2024-04-16 02:29:20 +0200

    committed

--- database_engine/src/main/java/db2/Tool.java
+++ database_engine/src/main/java/db2/Tool.java
@@ -1,9 +1,12 @@
 package db2;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class Tool {
     public static void initializeMetaData() {
@@ -27,4 +30,22 @@ public class Tool {
             }
         }
     }
+    public static void WriteInFile(Hashtable<String, String> htblColNameType, String strTableName,
+    String strClusteringKeyColumn) {
+    String filePath = "./metadata.csv"; 
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+        for (Map.Entry<String, String> entry : htblColNameType.entrySet()) {
+        String key = entry.getKey();
+        String value = entry.getValue();
+        boolean flag = key.equals(strClusteringKeyColumn);
+        writer.write(strTableName + ',' + key + ',' + value + ',' + flag + ",null,null");
+        writer.newLine(); 
+    }
+
+    } catch (Exception E) {
+     System.out.println("Failed to update metadata.csv!");
+    E.printStackTrace();
+    }
+    }
+
 }
